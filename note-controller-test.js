261bbc982269395e202e33f01c065f09be66c4d9
josh nickson <jnickson1506@gmail.com>
2018-09-19 10:42:07 +0100

    test drive creation of note controller

---
+++ note-controller-test.js
@@ -0,0 +1,7 @@
+(function(exports) {
+  function testNoteController() {
+    var noteController = new NoteController()
+    assert.isTrue(noteController)
+  }
+  testNoteController()
+})(this)
--- note-controller.js
+++ note-controller.js
@@ -9,8 +9,27 @@
   NoteController.prototype.changeGreeting = function() {
     var elem = document.getElementById('app')
     elem.innerHTML = "howdy"
-  }
+  }()
 
-    exports.NoteController = NoteController
+  NoteController.prototype.listItems = function() {
+  
+    var note1 = new Note('hello world')
+    var note2 = new Note('congee')
+    var note3 = new Note('door')
+
+    var noteList = new NoteList()
+    noteList.add(note1.text)
+    noteList.add(note2.text)
+    noteList.add(note3.text)
+    
+    var noteListView = new NoteListView()
+    var listItems = document.getElementById('list-items')
+    console.log(noteListView);
+    console.log("help");
+    
+    listItems.innerHTML = noteListView.htmlize(noteList.show())
+  }()
+
+  exports.NoteController = NoteController
 })(this)
 
