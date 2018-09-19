33ff331f379f78558d8f53149ed517c80bd19f00
josh nickson <jnickson1506@gmail.com>
2018-09-19 11:54:36 +0100

    add test for instantiating controller

--- note-controller.js
+++ note-controller.js
@@ -3,32 +3,24 @@
   function NoteController() {
     this._noteModel = new Note();
     this._noteListModel = new NoteListModel();
-    this._noteListView= new NoteListView();
+    this._noteListView = new NoteListView();
   }
 
-  NoteController.prototype.changeGreeting = function() {
-    var elem = document.getElementById('app')
-    elem.innerHTML = "howdy"
-  }()
-
   NoteController.prototype.listItems = function() {
-  
+    console.log('hello')
     var note1 = new Note('hello world')
     var note2 = new Note('congee')
     var note3 = new Note('door')
-    console.log(this._noteListModel)
 
     this._noteListModel.add(note1.text)
     this._noteListModel.add(note2.text)
     this._noteListModel.add(note3.text)
-   
     
     var listItems = document.getElementById('list-items')
-    console.log(noteListView);
     console.log("help");
     
-    listItems.innerHTML = this._noteListView.htmlize(noteList.show())
-  }()
+    listItems.innerHTML = this._noteListView.htmlize(this._noteListModel.show())
+  }
 
   exports.NoteController = NoteController
 })(this)
--- tests/note-list-test.js
+++ tests/note-list-test.js
@@ -1,6 +1,6 @@
 (function (exports) {
   function testNotesListView() {
-    var noteList = new NoteList
+    var noteList = new NoteListModel
     noteList.add('new note')
     assert.isTrue(noteList.notes.includes('new note'))
   }  
