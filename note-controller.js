b359412b14404dab68a0f47cabdd73d6dcf319d8
josh nickson <jnickson1506@gmail.com>
2018-09-20 10:41:33 +0100

    remove dependencies from note controller

--- note-controller.js
+++ note-controller.js
@@ -1,26 +1,13 @@
 (function(exports){
-
-  function NoteController() {
-    this._noteModel = new Note();
-    this._noteListModel = new NoteListModel();
-    this._noteListView = new NoteListView();
+  function NoteController(list, view = new NoteListView()) {
+    this._noteList = list;
+    this._noteListView = view;
   }
 
   NoteController.prototype.listItems = function() {
-    console.log('hello')
-    var note1 = new Note('haribo')
-    var note2 = new Note('congee')
-    var note3 = new Note('door')
-
-    this._noteListModel.add(note1.text)
-    this._noteListModel.add(note2.text)
-    this._noteListModel.add(note3.text)
-    
-    var listItems = document.getElementById('list-items')
-    console.log("help");
-    
-    listItems.innerHTML = this._noteListView.htmlize(this._noteListModel.show())
+    var listItems = document.getElementById('list-items');
+    listItems.innerHTML = this._noteListView.htmlize(this._noteList.show());
   }
 
-  exports.NoteController = NoteController
+  exports.NoteController = NoteController;
 })(this)
--- tests/note-controller-test.js
+++ tests/note-controller-test.js
@@ -5,13 +5,20 @@
   }
 
   function testNoteControllerInnerHTML() {
-    var noteController = new NoteController()
-    noteController.listItems()
-    var htmlString =  `<ul><li><div>haribo</div></li><li><div>congee</div></li><li><div>door</div></li></ul>`
-    innerHTML = document.getElementById('list-items').innerHTML  
-    assert.isTrue(innerHTML === htmlString)
+    var noteList = new NoteListModel();
+    var note1 = new Note('haribo');
+    var note2 = new Note('congee');
+    var note3 = new Note('door');
+    noteList.add(note1.text);
+    noteList.add(note2.text);
+    noteList.add(note3.text);
+    var noteController = new NoteController(noteList);
+    noteController.listItems();
+    var htmlString =  `<ul><li><div>haribo</div></li><li><div>congee</div></li><li><div>door</div></li></ul>`;
+    innerHTML = document.getElementById('list-items').innerHTML;
+    assert.isTrue(innerHTML === htmlString);
   }
-
-  testNoteController()
-  testNoteControllerInnerHTML()
+  
+  testNoteController();
+  testNoteControllerInnerHTML();
 })(this)
